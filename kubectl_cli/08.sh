e1d8e3c11193aa0104484735f28d02e3b9295fb4
oshrizivli <102049098+oshrizivli@users.noreply.github.com>
2022-03-21 21:54:24 +0200

    Delete kubectl_cli directory

--- kubectl_cli/08.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# This will remove the node-role.kubernetes.io/master taint from any nodes that have it
-kubectl taint nodes --all node-role.kubernetes.io/master-
-
-# apply yaml
-kubectl apply -f 08.yaml
-
--- kubectl_cli/14.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-kubectl apply -f 14.yaml
-
-kubectl run test-ns --image=busybox:1.28 --rm -it --restart=Never -- nslookup nginx-resolver-service
