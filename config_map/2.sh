a93eb568c3e83edcc5ff8568d9c3096ee0ea3250
oshrizivli <102049098+oshrizivli@users.noreply.github.com>
2022-03-21 21:56:42 +0200

    Delete config_map directory

--- config_map/2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Create a configmap named keyvalcfgmap and read data from the file config.txt
-kubectl create configmap keyvalcfgmap --from-env-file=config.txt
-
-# verify that configmap is created correctly
-kubectl get cm keyvalcfgmap -o yaml
