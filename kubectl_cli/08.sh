d98c977d6f966466a2ab4923c1584700b8b15616
oshrizivli <102049098+oshrizivli@users.noreply.github.com>
2022-03-24 19:18:11 +0200

    Update 08.sh

--- kubectl_cli/08.sh
+++ kubectl_cli/08.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+# first, lets checks nodes taints status
+kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints
+
 # This will remove the node-role.kubernetes.io/master taint from any nodes that have it
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # apply yaml
 kubectl apply -f 08.yaml
 
+# check that our pod was created on the master node
+kubectl get pods -owide
