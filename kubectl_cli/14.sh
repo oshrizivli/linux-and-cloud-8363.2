99e37884cbe7bccf9e320b461c64251f20828f7d
oshrizivli <102049098+oshrizivli@users.noreply.github.com>
2022-03-24 19:21:24 +0200

    Update 14.sh

--- kubectl_cli/14.sh
+++ kubectl_cli/14.sh
@@ -2,4 +2,5 @@
 
 kubectl apply -f 14.yaml
 
+# Test that you are able to look up the service and pod names from within the cluster
 kubectl run test-ns --image=busybox:1.28 --rm -it --restart=Never -- nslookup nginx-resolver-service
