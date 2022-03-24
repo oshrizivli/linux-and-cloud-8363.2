bf765ebbb8a220e92a11b205c04052acd2ca7cc7
oshrizivli <102049098+oshrizivli@users.noreply.github.com>
2022-03-24 19:20:47 +0200

    Update 13.sh

--- kubectl_cli/13.sh
+++ kubectl_cli/13.sh
@@ -5,22 +5,19 @@ kubectl apply -f 13.yaml
 
 NGINX_DEPLOY="nginx-deploy"
 
+# Record the version
 pod_image=$(kubectl describe $(kubectl get pods -o=name | grep $NGINX_DEPLOY) | grep "Image:")
 echo "$NGINX_DEPLOY currently using $pod_image"
 
+# upgrade the deployment to version 1.17 using rolling update
 kubectl set image deployments/$NGINX_DEPLOY $NGINX_DEPLOY=nginx:1.17 
 
 while kubectl get pods | grep ContainerCreating; do
   sleep 1
 done
 
+# ecord the changes for the image upgrade
 pod_image=$(kubectl describe $(kubectl get pods -o=name | grep $NGINX_DEPLOY) | grep "Image:")
 echo "$NGINX_DEPLOY currently using $pod_image"
 
 kubectl rollout status deployments/$NGINX_DEPLOY
-
-
-
-
-
-
